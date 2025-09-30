c9c701b1bdbe5c543f96ffc5f7cbc128121a564a
tharun <tharunsathy@gmail.com>
2025-09-30 18:55:58 +0530

    screen-time

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'screen-time-analytics',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'screen-time-analytics',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
